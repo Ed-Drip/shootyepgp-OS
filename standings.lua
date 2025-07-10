29a1824a6e36f8de486922c498cafe6ab00d8c99
Dmitry Repetsky <aka_dimis@yahoo.com>
2025-07-10 12:57:41 +0300

    filter standings to show only over 100 ep

--- standings.lua
+++ standings.lua
@@ -392,7 +392,7 @@ function sepgp_standings:BuildStandingsTable()
     local armor_class = self:getArmorClass(class)
 		rank = sepgp:parseRank(name,officernote) or rank
 		local rank_idx = sepgp:rankPrio_index(rank, 'MS', ep) or 1000
-		if ep>0 and ((not sepgp_raidonly) or next(r) and r[name]) then
+		if ep > 100 and ((not sepgp_raidonly) or next(r) and r[name]) then
 			table.insert(t,{name,class,armor_class,rank,ep,gp,ep/gp,rank_idx})
 		end
     -- if ep > 0 then
